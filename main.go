ae9d4be1a4ecfab11a16f39176afcb698f1ac41d
lachuginTech <vladimir.lachugin.tech@gmail.com>
2024-09-20 12:21:30 +0300

    Update main.go

--- main.go
+++ main.go
@@ -11,7 +11,8 @@ func main() {
 		}
 		transactions = append(transactions, transaction)
 	}
-	fmt.Println(transactions)
+	balance := calculateBalance(transactions)
+	fmt.Printf("Total balance: %.2f\n", balance)
 
 }
 
@@ -20,4 +21,12 @@ func scanTransaction() float64 {
 	fmt.Print("Enter transaction amount: ")
 	fmt.Scan(&amount)
 	return amount
+}
+
+func calculateBalance(transactions []float64) float64 {
+	balance := 0.0
+	for _, transaction := range transactions {
+		balance += transaction
+	}
+	return balance
 }
